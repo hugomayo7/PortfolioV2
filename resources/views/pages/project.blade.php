5ffe2f86df6c77d3fdb37d17b51d647299d74829
Mayonobe Hugo <hugomayonobe@gmail.com>
2023-01-16 21:48:24 +0100

    fix

--- resources/views/pages/project.blade.php
+++ resources/views/pages/project.blade.php
@@ -36,7 +36,76 @@
     <div class="cavani_tm_left_border"></div>
     <div class="cavani_tm_right_border"></div>
 
-    @include('components.header')
+    <!-- MOBILE MENU -->
+    <div class="cavani_tm_topbar">
+        <div class="topbar_inner">
+            <div class="logo" data-type="image">
+                <!-- You can use text or image as logo. data-type values are: "image" and "text"  -->
+                <a class="image" href="#"><img src="{{asset('img/logo/dark.png')}}" alt=""/></a>
+                <a class="text" href="#"><span>Mayonobe</span></a>
+            </div>
+            <div class="trigger">
+                <div class="hamburger hamburger--slider">
+                    <div class="hamburger-box">
+                        <div class="hamburger-inner"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="cavani_tm_mobile_menu">
+        <div class="inner">
+            <div class="wrapper">
+                <div class="avatar">
+                    <div class="image" data-img-url="/img/about/1.jpg"></div>
+                </div>
+                <div class="menu_list">
+                    <ul class="transition_link">
+                        <li class="active"><a href="#home">Home</a></li>
+                        <li><a href="#about">About</a></li>
+                        <li><a href="#portfolio">Portfolio</a></li>
+                        <li><a href="#service">Service</a></li>
+                        <li><a href="#news">News</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                    </ul>
+                </div>
+                <div class="social">
+                    <ul>
+                        <li><a href="#"><img class="svg" src="{{asset('img/svg/social/facebook.svg')}}" alt=""/></a></li>
+                        <li><a href="#"><img class="svg" src="{{asset('img/svg/social/twitter.svg')}}" alt=""/></a></li>
+                        <li><a href="#"><img class="svg" src="{{asset('img/svg/social/instagram.svg')}}" alt=""/></a></li>
+                        <li><a href="#"><img class="svg" src="{{asset('img/svg/social/dribbble.svg')}}" alt=""/></a></li>
+                        <li><a href="#"><img class="svg" src="{{asset('img/svg/social/tik-tok.svg')}}" alt=""/></a></li>
+                    </ul>
+                </div>
+                <div class="copyright">
+                    <p>Copyright &copy; 2022</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- /MOBILE MENU -->
+
+    <!-- HEADER -->
+    <div class="cavani_tm_header">
+        <div class="logo" data-type="text">
+            <!-- You can use text or image as logo. data-type values are: "image" and "text"  -->
+            <a class="image" href="/"><img src="{{asset('img/logo/dark.png')}}" alt=""/></a>
+            <a class="text" href="/"><span>MAYONOBE</span></a>
+        </div>
+        <div class="menu">
+            <ul class="transition_link">
+                <li><a href="/#home">Home</a></li>
+                <li><a href="/#about">About</a></li>
+                <li class="active"><a href="/#portfolio">Portfolio</a></li>
+                <li><a href="/#service">Service</a></li>
+                <li><a href="/#news">News</a></li>
+                <li><a href="/#contact">Contact</a></li>
+            </ul>
+            <span class="ccc"></span>
+        </div>
+    </div>
+    <!-- /HEADER -->
 
     <!-- /CONTENT -->
     <div class="cavani_tm_mainpart">
