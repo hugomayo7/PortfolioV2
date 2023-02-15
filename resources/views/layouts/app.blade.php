374e85396f287a08e1a1b0b40fc56383d426986a
Mayonobe Hugo <hugomayonobe@gmail.com>
2023-02-15 11:09:15 +0100

    Revert "[+](analytics) link google analytics"
    This reverts commit 1777989d0d44bf5803c9b3b1a50ba21ca96eb61d.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,16 +30,6 @@
     @livewireScripts
 </head>
 
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-D1NPWRD5JH"></script>
-<script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'G-D1NPWRD5JH');
-</script>
-
 <body>
 
 <!-- PRELOADER -->
